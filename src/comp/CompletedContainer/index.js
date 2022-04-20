4066f185804ae7eccefda2a067326e7aba898f75
Lorcan Mc <lorcan.jmc@gmail.com>
2022-04-20 12:05:50 +0100

    commented out copied alert

--- src/comp/CompletedContainer/index.js
+++ src/comp/CompletedContainer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CompletedContainer({ gridFormatting }) {
   function copyToClipboard() {
-      alert("Copied")
+    //   alert("Copied")
     const tiles = gridFormatting.map((nested) =>
       nested.map((tile) => {
         if (tile.backgroundColor === "rgb(80, 171, 92)") {
